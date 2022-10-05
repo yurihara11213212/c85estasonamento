9b88321762ec79694be5758fbfceb341755b26ef
yurihara11213212 <114710091+yurihara11213212@users.noreply.github.com>
2022-10-04 20:43:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,117 @@
-var mouseEvent = "empty";
-var lastPositionX, lastPositionY;
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    widthLine = 1;
-    canvas.addEventListener("mousedown", myMouseDown);
-    function myMouseDown(e)
-    {
-        //Addictonal Activity start
-        color = document.getElementById("color").value;
-        widthLine = document.getElementById("widthLine").value;
-        //Addictonal Activity ends
+greencar_width = 75;
+greencar_height = 100;
 
-        mouseEvent = "mouseDown";
-    }
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
 
-    canvas.addEventListener("mousemove", myMouseMove);
-    function myMouseMove(e)
-    {
-        PositionMouseX = e.clientX - canvas.offsetLeft;
-        PositionMouseY = e.clientY - canvas.offsetTop;
+//greencar_x = 75;
+//greencar_y = 325;
+greencar_x = 5;
+greencar_y= 225;
 
-        if(mouseEvent =="mouseDown"){
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=widthLine;
+function add() {
+	background_imgTag = new Image(); 				//defining a variable with a new image
+	background_imgTag.onload = uploadBackground; 	// setting a function, onloading this variable
+	background_imgTag.src = background_image;   	// load image
 
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + lastPositionX + "y = " + lastPositionY);
-        ctx.moveTo(lastPositionX, lastPositionY);
+	greencar_imgTag = new Image(); 				//defining a variable with a new image
+	greencar_imgTag.onload = uploadgreencar; 	// setting a function, onloading this variable
+	greencar_imgTag.src = greencar_image;  		 // load image
 
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + PositionMouseX + "y = " + PositionMouseY);
-        ctx.lineTo(PositionMouseX, PositionMouseY);
-        ctx.stroke();
-        }
+}
 
-        lastPositionX = PositionMouseX; 
-        lastPositionY = PositionMouseY;
-    }
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
 
-    canvas.addEventListener("mouseup", myMouseUp);
-    function myMouseUp(e)
-    {
-        mouseEvent = "mouseUP";
-    }
-
-    canvas.addEventListener("mouseleave", myMouseLeave);
-    function myMouseLeave(e)
-    {
-        mouseEvent = "mouseleave";
-    }
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, greencar_x,greencar_y, greencar_width, greencar_height);
+	
+}
 
 
-//Additional Activity
-function clearArea() {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+
+
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	
+
+		}
+		
+
+function up()
+{
+	if(greencar_y >=0)
+	{
+		greencar_y = greencar_y - 10;
+		console.log("When up arrow is pressed,  x = " + greencar_x + " | y = " +greencar_y);
+		 uploadBackground();
+		 uploadgreencar();
+		
+	}
+}
+
+function down()
+{
+	if(greencar_y <=350)
+	{
+		greencar_y =greencar_y+ 10;
+		console.log("When down arrow is pressed,  x = " + greencar_x + " | y = " +greencar_y);
+		uploadBackground();
+		uploadgreencar();
+		
+	}
+}
+
+function left()
+{
+	if(greencar_x >= 0)
+	{
+		greencar_x =greencar_x - 10;
+		console.log("When left arrow is pressed,  x = " + greencar_x + " | y = " +greencar_y);
+		uploadBackground();
+		 uploadgreencar();
+		
+	}
+}
+
+function right()
+{
+	if(greencar_x <= 750)
+	{
+		greencar_x =greencar_x + 10;
+		console.log("When right arrow is pressed,  x = " + greencar_x + " | y = " +greencar_y);
+		uploadBackground();
+		uploadgreencar();
+		
+   }
 }
